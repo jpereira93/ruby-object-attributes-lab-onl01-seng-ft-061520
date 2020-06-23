d6628f3bb16b4518c110fd97955ecc3e9f7164c4
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 03:07:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,13 +5,18 @@ class Person
   def name
     @name
   end
+  def jobs=(jobs)
+    @jobs = jobs
+  end
+  def jobs
+    @jobs
+  end
 
 end
 
 
 
 
-#
-# Person with names #name reads the name of the person from an instance variable @name
+
 # Person with jobs #job= writes the job of the person to an instance variable @job
 # Person with jobs #job reads the job of the person from an instance variable @job
