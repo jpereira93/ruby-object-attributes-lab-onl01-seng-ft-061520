68c31c021c381fa3e7602ae98650fbe0132eabf2
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 03:03:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ end
 
 
 
-Person is defined within lib/person.rb
+# Person is defined within lib/person.rb
 # Person with names #name= writes the name of the person to an instance variable @name
 # Person with names #name reads the name of the person from an instance variable @name
 # Person with jobs #job= writes the job of the person to an instance variable @job
